448360fd2239d41906db38206c3dcd337844442b
Kittypoda <odakrolstad@gmail.com>
2025-06-11 09:21:20 +0200

    header fixed -removed from landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; 
 import { FaHeart } from "react-icons/fa";
 import LevelLogo from "../assets/logo.png";
 
 function Header() {
+  const location = useLocation(); 
 
-  if (location.pathname === "/") return null;
-  
+  if (location.pathname === "/") return null; 
+
   return (
     <header className="flex items-center justify-between px-4 md:px-20 pt-8 mb-6">
-      <Link to="/" className="text-2xl font-bold">
-        <img src={LevelLogo}
-        alt="Level up Lounge logo" 
-        className="md:w-52 w-40 h-auto"
+      <Link to="/home" className="text-2xl font-bold">
+        <img
+          src={LevelLogo}
+          alt="Level up Lounge logo"
+          className="md:w-52 w-40 h-auto"
         />
       </Link>
-<button className="bg-gray p-2 rounded border border-yellow shadow-btnsolid">
-      <Link
-        to="/favourites"
-        className="text-2xl text-pink transition"
-        aria-label="View favourites"
-      >
-        <FaHeart />
-      </Link>
+      <button className="bg-gray p-2 rounded border border-yellow shadow-btnsolid">
+        <Link
+          to="/favourites"
+          className="text-2xl text-pink transition"
+          aria-label="View favourites"
+        >
+          <FaHeart />
+        </Link>
       </button>
     </header>
   );
 }
 
 export default Header;
+
 
