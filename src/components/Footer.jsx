190c322d7733686d56ed20218517e92f61968ee3
Kittypoda <odakrolstad@gmail.com>
2025-06-11 09:35:34 +0200

    footer joystick

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
+import LevelJoy from "../assets/gamingjoystick.png";
 
 function Footer() {
   return (
-    <footer className="bg-[#2E2D2D] h-40">
+    <footer className="bg-[#2E2D2D] h-56">
+      
+      <div className=" px-4 md:px-20 pt-10">
+        <img src={LevelJoy} 
+        alt="gaming joystick"
+        className="w-40 h-full" />
+      </div>
 
 <p className="text-sm pt-4 pl-4 md:pl-20 pt-32">Icon from <Link className="text-gray-600 text-sm underline" to="https://icons8.com/">Icons8</Link></p>
   
